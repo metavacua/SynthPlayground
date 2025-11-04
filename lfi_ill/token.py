0499a59573da02529aa218b0734fd2221fb3a7ec
metavacua <metavacua@gmail.com>
2025-11-03 20:47:31 -0800

    Delete lfi_ill directory

--- lfi_ill/token.py
+++
@@ -1,7 +0,0 @@
-class Token:
-    def __init__(self, type, value, pos):
-        self.type = type
-        self.value = value
-        self.pos = pos
-    def __repr__(self):
-        return f"Token({self.type}, {self.value}, {self.pos})"
